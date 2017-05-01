0d37d2ce31d1105c0e4c288acc1a67c7cadca0e4
varshinim <himavarshini.mittapally@gmail.com>
2017-04-30 18:30:56 -0700

    Updated gulpfile

--- src/main/resources/gulpfile.js
+++ src/main/resources/gulpfile.js
@@ -1,32 +1,35 @@
 var 
-	gulp = require('gulp'),
-	devBuild = (process.env.NODE_ENV !== 'production'),
-	newer = require('gulp-newer'),
-	htmlclean = require('gulp-htmlclean'),
+	gulp = require('gulp'), //reference gulp module
+	devBuild = (process.env.NODE_ENV !== 'production'), // sets a devBuild variable to true when running in development mode
+	Q = require('q'),
 	concat = require('gulp-concat'), //concat to single file
 	deporder = require('gulp-deporder'), //reorder JavaScript or CSS files in the stream
-	stripdebug = require('gulp-strip-debug'),
-	uglify = require('gulp-uglify'),
-  
-  folder = {
-    src: 'resources/',
-    build: 'dist/'
-  };
-
-
-gulp.task('build', function() {
-	sequence('clean')
-});
+	stripdebug = require('gulp-strip-debug'), //removes all console and debugging statements
+	uglify = require('gulp-uglify'), // minimize code
+	inject = require('gulp-inject'), //read file and inject
+	del = require('del'),
+	sequence = require('run-sequence'),
+    //folders
+    folder = {
+	    src: 'resources/',
+	    build: 'public/'
+	};
 
 // clean dist
 gulp.task('clean', function(){
-	
+	return del(['public/*']);
+});
+
+//build css
+gulp.task('images', function(cb) {
+   return gulp.src('static/images/**/*')
+     .pipe(gulp.dest(folder.build + 'static/images'));
 });
 
 // build js
 gulp.task('js', function(cb) {
-	var jsbuild = folder.build + 'js/',
-	jssrc = gulp.src(folder.src + 'js/**/*')
+	var jsbuild = folder.build + 'static/js/',
+	jssrc = gulp.src('static/js/**/*')
     .pipe(deporder())
     .pipe(concat('main.js'));
 
@@ -41,45 +44,64 @@ gulp.task('js', function(cb) {
 
 //build css
 gulp.task('css', function(cb) {
-	var postCssOpts = [
-       assets({ loadPaths: ['images/'] }),
-       autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-       mqpacker
-       ];
-
     if (!devBuild) {
      postCssOpts.push(cssnano);
     }
 
-    return gulp.src(folder.src + 'scss/main.scss')
-     .pipe(sass({
-       outputStyle: 'nested',
-       imagePath: 'images/',
-       precision: 3,
-       errLogToConsole: true
-    }))
-     .pipe(postcss(postCssOpts))
-     .pipe(gulp.dest(folder.build + 'css/'));
+    return gulp.src('static/css/**/*')
+     .pipe(concat('base.css'))
+     .pipe(gulp.dest(folder.build + 'static/css/'));
 });
 
-//build js
 gulp.task('html', function(cb) {
-	var htmlbuild = folder.build + 'templates/',
-    htmlsrc = gulp.src(folder.src + 'templates/**/*')
-      .pipe(newer(htmlbuild));
-
-	  // minify production code
-	if (!devBuild) {
-    htmlsrc = htmlsrc.pipe(htmlclean());
-	}
-	
-   return htmlsrc.pipe(gulp.dest(htmlbuild));
+    return gulp.src('templates/base.html')
+     .pipe(concat('base.html'))
+     .pipe(gulp.dest(folder.build + 'templates'));
 });
 
+var injectJS = function () {
+	var defer = Q.defer();
+	var stream = gulp.src('public/templates/base.html')
+    .pipe(inject(gulp.src(folder.build + 'static/js/*.js', {read: false}), {ignorePath: 'public'}))
+    .pipe(gulp.dest(folder.build + 'templates'));
+	stream.on('end', function() {
+		defer.resolve(stream);
+	});
+	return defer.promise;
+}
+
+// inject js
 gulp.task('js:inject', function(){
-	
+	return injectJS();
 });
 
+var injectCSS = function () {
+	var defer = Q.defer();
+	var stream = gulp.src('public/templates/base.html')
+    .pipe(inject(gulp.src(folder.build + 'static/css/*.css', {read: false}), {selfClosingTag: true, ignorePath: 'public'}))
+    .pipe(gulp.dest(folder.build + 'templates'));
+	stream.on('end', function() {
+		defer.resolve(stream);
+	});
+	return defer.promise;
+}
+
+// inject css
 gulp.task('css:inject', function(){
-	
+	return injectCSS();
 });
+
+gulp.task('build', function() {
+	sequence('clean', ['html', 'css', 'js', 'images'], 'css:inject', 'js:inject');
+});
+
+gulp.task('build:watch', function() {
+	gulp.watch('static/images/*', ['images']);
+	gulp.watch('static/css/*.css', ['css', 'css:inject']);
+	gulp.watch('static/js/*.js', ['js', 'js:inject']);
+	gulp.watch('templates/*.html', ['build']);
+});
+
+
+
+
